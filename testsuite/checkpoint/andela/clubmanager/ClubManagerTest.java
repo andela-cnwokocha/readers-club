5ff1e5410c051a4beeac27c979329263a4475643
andela-cnwokocha <chidiebere.nwokocha@andela.com>
2015-11-27 19:55:00 +0100

    Update the project scope to remove file managing classes

--- testsuite/checkpoint/andela/clubmanager/ClubManagerTest.java
+++ testsuite/checkpoint/andela/clubmanager/ClubManagerTest.java
@@ -1,63 +1,36 @@
 package checkpoint.andela.clubmanager;
 
-import checkpoint.andela.main.Book;
 import org.junit.Test;
-
-import java.io.File;
-import java.nio.file.Paths;
-
-import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 /**
- * Created by chidi on 11/26/15.
+ * Created by chidi on 11/27/15.
  */
 public class ClubManagerTest {
 
   @Test
-  /* check the number of books in the library */
   public void testGetLibrarySize() throws Exception {
-    ClubManager club = new ClubManager();
-    Book book = new Book("Ikemefuna", "Chidiebere", 21, "ISBN-5786-686");
-    club.addBookToLibrary(book);
-    assertEquals(club.getLibrarySize(),1);
 
-  }
-   @Test
-  //check if a book is added to the book list with details about the book
-  public void testAddBookToLibrary() throws Exception {
-     try {
-       ClubManager club = new ClubManager();
-       Book book = new Book("Ikemefuna", "Chidiebere", 21, "ISBN-5786-686");
-       Book book2 = new Book("Arrowhead", "Dayo Ajere", 3, "ISBN-576-79-897");
-       assertTrue(club.addBookToLibrary(book2));
-       //assertTrue(club.addBookToLibrary(book));
-       assertEquals(club.getLibrarySize(), 1);
-     }catch(Exception e) {
-       e.printStackTrace();
-     }
-   }
-  @Test
-  // check if an .txt file about the book is created
-  public void testCreateAboutBookFile() throws Exception {
-    ClubManager club = new ClubManager();
-    Book book = new Book("Lebweka", "Chidiebere", 21, "ISBN-5786-686");
-    try {
-      club.addBookToLibrary(book);
-      String directory = Paths.get(".").toAbsolutePath().normalize().toString();
-      String bookName = book.getBookName();
-      boolean fileExists = new File(directory, bookName + ".txt").exists();
-      assertTrue(fileExists);
-    }catch(Exception e) {
-      e.printStackTrace();
-    }
   }
 
   @Test
-  public void sayhelloTest() throws Exception {
-    System.out.print("Hello my friend");
-    assertEquals(3,3);
+  public void testGetNumberOfMembers() throws Exception {
+
   }
 
+  @Test
+  public void testAddBookToList() throws Exception {
+
+  }
+
+  @Test
+  public void testAddMemberToList() throws Exception {
+
+  }
+
+  @Test
+  public void testGetSizeOfQueue() throws Exception {
+
+  }
 }
